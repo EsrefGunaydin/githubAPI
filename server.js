c8d55812dd00af77f29b7b69123cd669b518db83
Esref Gunaydin <esrefgunaydin@gmail.com>
2021-10-24 12:59:31 -0700

    index.html added and server.js updated

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const http = require("http");
+const fs = require("fs");
+const axios = require("axios");
 const PORT = 8000;
 
 http
   .createServer((req, res) => {
-    console.log("Server is working on 8000");
+    //Basic route for form
+    if (req.url === "/") {
+      fs.readFile("./index.html", null, (err, data) => {
+        if (err) {
+          res.writeHead(404);
+          res.write("File was not found");
+        } else {
+          res.write(data);
+        }
+        res.end();
+      });
+    }
+
+    console.log(`Server is working on port: ${PORT}`);
   })
   .listen(PORT);
